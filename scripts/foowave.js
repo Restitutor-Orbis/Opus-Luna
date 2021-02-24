899933f7e248e011aa391b25cfa8b587ed9b9d5d
Restitutor-Orbis <65425000+Restitutor-Orbis@users.noreply.github.com>
2021-02-24 18:40:04 +0100

    Update foowave.js

--- scripts/foowave.js
+++ scripts/foowave.js
@@ -5,7 +5,7 @@ var songPath = "./themes/" + theme + "/songs/" + rndSong + ".mp3";
 
 var volume = 0.1;
 
-console.log("Volume: " + volume);
+updateDisplay(theme, rndSong);
 
 var wavesurfer = WaveSurfer.create({
     container: document.querySelector('#waveform'),
@@ -27,6 +27,7 @@ wavesurfer.on('finish', function () {
     songPath = "./themes/" + theme + "/songs/" + rndSong + ".mp3";
     wavesurfer.load(songPath);
     playAudio(wavesurfer);
+    updateDisplay(theme, rndSong);
 });
 
 wavesurfer.on('dblclick', function () {
@@ -34,6 +35,7 @@ wavesurfer.on('dblclick', function () {
 });
 
 playAudio(wavesurfer);
+
 
 function pickTheme() {
     return "0";
@@ -43,12 +45,49 @@ function randomSong(theme) {
     return random = Math.floor(Math.random() * 10) + 1;
 }
 
-function updateDisplay(theme, song, ) {
-
-}
-
 function playAudio(waveSurfer) {
     wavesurfer.on('ready', function () {
         wavesurfer.play();
     });
+}
+
+function updateDisplay(theme, song) {
+    if(theme == 0) {
+        document.getElementById("subtitle").innerHTML = "Redneck Vikings From Hell - Æther Realm";
+        switch(song) {
+            case 1:
+                document.getElementById("title").innerHTML = "Redneck Vikings From Hell";
+                break;
+            case 2:
+                document.getElementById("title").innerHTML = "Goodbye";
+                break;
+            case 3:
+                document.getElementById("title").innerHTML = "Lean Into the Wind";
+                break;
+            case 4:
+                document.getElementById("title").innerHTML = "Hunger";
+                break;
+            case 5:
+                document.getElementById("title").innerHTML = "Guardian";
+                break;
+            case 6:
+                document.getElementById("title").innerHTML = "One Hollow Word";
+                break;
+            case 7:
+                document.getElementById("title").innerHTML = "She's Back";
+                break;
+            case 8:
+                document.getElementById("title").innerHTML = "Slave to the Riff";
+                break;
+            case 9:
+                document.getElementById("title").innerHTML = "Cycle";
+                break;
+            case 10:
+                document.getElementById("title").innerHTML = "TMHC";
+                break;
+            case 11:
+                document.getElementById("title").innerHTML = "Craft and the Creator";
+                break;
+        }
+    }
 }
