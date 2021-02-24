d8feffd9876a4125484735d941c303c1c4ae98f4
Restitutor-Orbis <65425000+Restitutor-Orbis@users.noreply.github.com>
2021-02-24 15:31:07 +0100

    Update foowave.js

--- scripts/foowave.js
+++ scripts/foowave.js
@@ -15,7 +15,7 @@ var wavesurfer = WaveSurfer.create({
     barGap: 1
 });
 
-wavesurfer.load('http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3')
+wavesurfer.load(songPath);
 
 wavesurfer.on('ready', function () {
     wavesurfer.play();
